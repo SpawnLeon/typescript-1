36a7f1b4d1ee20a02ccf73cfcbadc000505706f8
Alexander Leonov <spawnleon@gmail.com>
2024-02-09 08:28:54 +0300

    implement 4-dummy-api

---
+++ 4-dummy-api/types.js
@@ -0,0 +1,46 @@
+var Gender;
+(function (Gender) {
+    Gender["Male"] = "male";
+    Gender["Female"] = "female";
+})(Gender || (Gender = {}));
+var BloodGroup;
+(function (BloodGroup) {
+    BloodGroup["APositive"] = "A+";
+    BloodGroup["ANegative"] = "A-";
+    BloodGroup["BPositive"] = "B+";
+    BloodGroup["BNegative"] = "B-";
+    BloodGroup["ABPositive"] = "AB+";
+    BloodGroup["ABNegative"] = "AB-";
+    BloodGroup["OPositive"] = "O+";
+    BloodGroup["ONegative"] = "O-";
+})(BloodGroup || (BloodGroup = {}));
+var CardType;
+(function (CardType) {
+    CardType["Maestro"] = "maestro";
+    CardType["Mastercard"] = "mastercard";
+    CardType["VisaElectron"] = "visa-electron";
+    CardType["AmericanExpress"] = "americanexpress";
+    CardType["DinersClubCarteBlanche"] = "diners-club-carte-blanche";
+    CardType["JCB"] = "jcb";
+    CardType["Bankcard"] = "bankcard";
+    CardType["InstaPayment"] = "instapayment";
+    CardType["Solo"] = "solo";
+    CardType["Switch"] = "switch";
+})(CardType || (CardType = {}));
+var HairColor;
+(function (HairColor) {
+    HairColor["Black"] = "Black";
+    HairColor["Blond"] = "Blond";
+    HairColor["Brown"] = "Brown";
+    HairColor["Chestnut"] = "Chestnut";
+    HairColor["Auburn"] = "Auburn";
+})(HairColor || (HairColor = {}));
+var HairType;
+(function (HairType) {
+    HairType["Strands"] = "Strands";
+    HairType["Curly"] = "Curly";
+    HairType["VeryCurly"] = "Very curly";
+    HairType["Wavy"] = "Wavy";
+    HairType["Straight"] = "Straight";
+})(HairType || (HairType = {}));
+export {};
